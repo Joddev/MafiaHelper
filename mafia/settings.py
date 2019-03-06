c081bd844a3bdaf9a9f09b8d46e18fc416072a50
chy <whgksdyd112@naver.com>
2019-03-06 14:27:03 +0900

    WhiteNoise error fixed

--- mafia/settings.py
+++ mafia/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -189,5 +190,6 @@ LOGGING = {
     },
 }
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Activate Django-Heroku.
 django_heroku.settings(locals())
