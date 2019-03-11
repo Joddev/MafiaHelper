95d8503f2a165c234ac4eae616643c00cca98f58
chy <whgksdyd112@naver.com>
2019-03-11 17:22:46 +0900

    Remove unused files

--- game/urls.py
+++ game/urls.py
@@ -4,9 +4,5 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('new', views.room_create, name='room_new'),
-    path('room/<int:pk>', views.room_detail, name='room_detail'),
-    url(r'^user/(?P<pk>\d+)/update/$', views.user_update, name='user_update'),
-    url(r'^test/$', views.test, name='test'),
+    path('', views.index, name='index')
 ]
